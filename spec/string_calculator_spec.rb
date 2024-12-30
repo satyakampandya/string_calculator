90809217242cd74fea5dda800935f07a5562da71
Satyakam Pandya <satyakampandya@gmail.com>
2024-12-30 22:40:24 +0530

    Initial Commit

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe StringCalculator do
+  it "has a version number" do
+    expect(StringCalculator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
